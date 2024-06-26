dc72e9800c88523ad1d0a73f00dda635400495f6
MarcoPanz <marcopanzan23@gmail.com>
2024-06-26 18:40:04 +0200

    first commit

--- app/Http/Controllers/AnimeController.php
+++ app/Http/Controllers/AnimeController.php
@@ -7,20 +7,21 @@ use Illuminate\Support\Facades\Http;
 
 class AnimeController extends Controller
 {
-    public function welcome() {
-    $response = Http::get('https://api.jikan.moe/v4/genres/anime')->json();
-    
-    return view('welcome', ['data' => $response['data']]);
+    public function welcome()
+    {
+        $response = Http::get('https://api.jikan.moe/v4/genres/anime')->json();
+
+        return view('welcome', ['data' => $response['data']]);
     }
 
-    public function genre($id){
-        
+    public function genre($id)
+    {
         $response = Http::get('https://api.jikan.moe/v4/anime?genres=' . $id)->json();
         return view('list-genre', ['data' => $response['data']]);
     }
 
-    public function detail($id){
-        
+    public function detail($id)
+    {
         $response = Http::get('https://api.jikan.moe/v4/anime/' . $id)->json();
         return view('detail', ['data' => $response['data']]);
     }
